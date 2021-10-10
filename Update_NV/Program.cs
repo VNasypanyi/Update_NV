d556bea45d30e202baf7d7adade103dc7cea38e4
VadymNasypanyi <vadym.nasypanyi@gmail.com>
2021-10-11 00:38:01 +0300

    visual studio testing git

--- Update_NV/Program.cs
+++ Update_NV/Program.cs
@@ -10,6 +10,8 @@ namespace Update_NV
         {
             IWebDriver driver = new ChromeDriver();
             driver.Url = "https://nuget-tutorial.net/en/tutorial/100010/package-manager-ui";
+
+            //test
         }
     }
 }
