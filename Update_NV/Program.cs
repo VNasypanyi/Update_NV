3f733f3b99fdb023a0ab8e1c963587cd076ae929
VadymNasypanyi <vadym.nasypanyi@gmail.com>
2021-10-11 00:36:01 +0300

    Add project files.

---
+++ Update_NV/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Update_NV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://nuget-tutorial.net/en/tutorial/100010/package-manager-ui";
+        }
+    }
+}
