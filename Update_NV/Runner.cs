5ff525ff9a1af880c63fe206ebae8449d21ef16a
VadymNasypanyi <vadym.nasypanyi@gmail.com>
2021-10-17 19:41:33 +0300

    refactoring

---
+++ Update_NV/Runner.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using System.Threading;
+using Serilog;
+using OpenQA.Selenium.Internal;
+using OpenQA.Selenium.Support.UI;
+using Serilog.Sinks.SystemConsole;
+using Serilog.Sinks.File;
+
+namespace Update_NV
+{
+    public class Runner : Baseclass
+    {
+       
+    }
+}
