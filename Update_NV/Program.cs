6f449515885db7e8b7b27ebc988e6698da4381ed
VadymNasypanyi <vadym.nasypanyi@gmail.com>
2021-10-17 16:06:47 +0300

    test

--- Update_NV/Program.cs
+++ Update_NV/Program.cs
@@ -4,15 +4,14 @@ using OpenQA.Selenium.Chrome;
 
 namespace Update_NV
 {
-    class Program
+    class runner
     {
         static void Main(string[] args)
         {
             IWebDriver driver = new ChromeDriver();
             driver.Url = "https://nuget-tutorial.net/en/tutorial/100010/package-manager-ui";
 
-            IWebDriver driver1 = new ChromeDriver();
-            driver.Url = "https://nuget-tutorial.net/en/tutorial/100010/package-manager-ui";
+         
 
         }
     }
