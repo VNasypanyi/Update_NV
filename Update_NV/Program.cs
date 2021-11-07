fb6c05f4da4925fd32cbdd6621c2e26465b7de36
VadymNasypanyi <vadym.nasypanyi@gmail.com>
2021-11-07 18:21:48 +0200

    upd

--- Update_NV/Program.cs
+++ Update_NV/Program.cs
@@ -8,10 +8,8 @@ namespace Update_NV
     {
         static void Main(string[] args)
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://nuget-tutorial.net/en/tutorial/100010/package-manager-ui";
-
-       ////  retest
+           
+        
 
         }
     }
